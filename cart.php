ccbe2aae8ca0b82027b391ba7971b20a956f89c3
nyndaa <raharjiansustanadiawijayanti@gmail.com>
2026-07-05 09:01:58 +0700

    mengubah style keranjang

--- cart.php
+++ cart.php
@@ -2,129 +2,204 @@
 
 include 'connect.php';
 
+// hapus produk dari keranjang
+if(isset($_GET['hapus']))
+{
+    $kode = $_GET['hapus'];
+
+    mysqli_query(
+        $connect,
+        "DELETE FROM cart
+        WHERE product_code='$kode'"
+    );
+
+    header("Location: cart.php");
+    exit;
+}
+
+// tambah produk ke keranjang
 if(isset($_GET['add']))
 {
-    $kode=$_GET['add'];
+    $kode = $_GET['add'];
 
-    $cek=mysqli_query($connect,
-    "SELECT * FROM cart
-    WHERE product_code='$kode'");
+    $cek = mysqli_query(
+        $connect,
+        "SELECT * FROM cart
+        WHERE product_code='$kode'"
+    );
 
-    if(mysqli_num_rows($cek)>0)
+    if(mysqli_num_rows($cek) > 0)
     {
-        mysqli_query($connect,
-        "UPDATE cart
-        SET qty=qty+1
-        WHERE product_code='$kode'");
+        mysqli_query(
+            $connect,
+            "UPDATE cart
+            SET qty = qty + 1
+            WHERE product_code='$kode'"
+        );
     }
     else
     {
-        mysqli_query($connect,
-        "INSERT INTO cart(product_code,qty)
-        VALUES('$kode',1)");
+        mysqli_query(
+            $connect,
+            "INSERT INTO cart(product_code, qty)
+            VALUES('$kode', 1)"
+        );
     }
 
-    header("Location:cart.php");
+    header("Location: cart.php");
+    exit;
 }
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Keranjang</title>
-<link rel="stylesheet" href="assets/style.css">
+    <title>Keranjang Belanja</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 
 <body>
+<div class="container-keranjang">
+    <div class="hero-keranjang">
 
-<h1 style="padding:20px">
-🛒 Keranjang Belanja
-</h1>
+    <div class="hero-keranjang-content">
 
-<table>
+        <span class="badge-keranjang">
+            BEAUTY SKINCARE OFFICIAL STORE
+        </span>
 
-<tr>
-<th>Produk</th>
-<th>Harga</th>
-<th>Qty</th>
-<th>Total</th>
-</tr>
+        <h1 class="judul-keranjang">
+             🌸 Beauty Shopping Cart🌸
+        </h1>
 
-<?php
+        <p class="subjudul-keranjang">
+            Periksa kembali produk pilihan Anda sebelum melanjutkan ke proses checkout.
+            Pastikan jumlah dan produk yang dipilih sudah sesuai kebutuhan skincare Anda.
+        </p>
 
-$total=0;
+    </div>
 
-$sql="
-SELECT
-product.name,
-product.price,
-cart.qty
+</div>
 
-FROM cart
+   <table class="tabel-keranjang">
 
-INNER JOIN product
-ON cart.product_code=product.code
-";
+    <tr>
+        <th>No</th>
+        <th>Gambar</th>
+        <th>Produk</th>
+        <th>Harga</th>
+        <th>Jumlah</th>
+        <th>Subtotal</th>
+        <th>Aksi</th>
+    </tr>
 
-$result=mysqli_query($connect,$sql);
+    <?php
 
-while($row=mysqli_fetch_assoc($result))
-{
-    $subtotal=
-    $row['price']*$row['qty'];
+    $total = 0;
 
-    $total+=$subtotal;
-?>
+    $sql = "
+    SELECT
+    product.name,
+    product.price,
+    product.image,
+    cart.qty,
+    cart.product_code
 
-<tr>
+    FROM cart
 
-<td><?php echo $row['name']; ?></td>
+    INNER JOIN product
+    ON cart.product_code = product.code
+    ";
 
-<td>
-Rp <?php echo number_format($row['price']); ?>
-</td>
+    $result = mysqli_query($connect, $sql);
 
-<td>
-<?php echo $row['qty']; ?>
-</td>
+    $no = 1;
 
-<td>
-Rp <?php echo number_format($subtotal); ?>
-</td>
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $subtotal = $row['price'] * $row['qty'];
 
-</tr>
+        $total += $subtotal;
+    ?>
 
-<?php
-}
-?>
+    <tr>
 
-<tr>
+        <td>
+            <?php echo $no++; ?>
+        </td>
 
-<td colspan="3">
-<b>Total Belanja</b>
-</td>
+        <td>
+           <img
+src="images/<?php echo $row['image']; ?>"
+alt="<?php echo $row['name']; ?>"
+class="gambar-produk-keranjang">
+        </td>
 
-<td>
-<b>
-Rp <?php echo number_format($total); ?>
-</b>
-</td>
+        <td class="nama-produk-keranjang">
+            <?php echo $row['name']; ?>
+        </td>
 
-</tr>
+        <td>
+            Rp <?php echo number_format($row['price']); ?>
+        </td>
+
+        <td>
+            <?php echo $row['qty']; ?>
+        </td>
+
+        <td>
+            Rp <?php echo number_format($subtotal); ?>
+        </td>
+
+        <td>
+            <a
+            href="cart.php?hapus=<?php echo $row['product_code']; ?>"
+            class="btn-hapus-keranjang"
+            onclick="return confirm('Hapus produk dari keranjang?')">
+                Hapus
+            </a>
+        </td>
+
+    </tr>
+
+    <?php
+    }
+    ?>
+
+    <tr class="baris-total-keranjang">
+
+        <td colspan="5">
+            <b>Total Belanja</b>
+        </td>
+
+        <td colspan="2">
+            <b class="total-harga-keranjang">
+                Rp <?php echo number_format($total); ?>
+            </b>
+        </td>
+
+    </tr>
 
 </table>
 
-<center>
+ <div class="aksi-keranjang">
 
-<br>
+    <div>
+        <a href="index.php" class="btn-kembali-keranjang">
+            ← Kembali Belanja
+        </a>
+    </div>
 
-<a href="checkout.php"
-class="btn-cart">
-Checkout
-</a>
+    <div>
+        <a href="checkout.php" class="btn-checkout-keranjang">
+            Checkout →
+        </a>
+    </div>
 
-</center>
-
+</div>
+</div>
 </body>
+
 </html>
